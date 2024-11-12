ff586cf85ac32de410fdca74d1a966e98e7af0c0
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-11-12 20:00:29 +0530

    added

--- uams/uams/urls.py
+++ uams/uams/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('',views.index),
+    path('admin/', admin.site.urls),
 ]
