4b2b4d14efdba04f6863f4d2a3d273fffeeacb76
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-11-13 21:40:56 +0530

    main page created

--- main/views.py
+++ main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index2(request,name):
-    return HttpResponse(f'Hi {name}')
+    return HttpResponse(f'<h1>Hi {name}</h1>')
 def index(request):
     return render(request,"index.html")
