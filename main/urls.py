4a052784bd14a076e805d67ea8dface5186ad343
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-11-14 21:37:08 +0530

    small change

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('edit',views.edit,name="edit"),
+    path('<str:name>',views.student,name="report"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def index2(request,name):
-    return HttpResponse(f'<h1>Hi {name}</h1>')
 def index(request):
     return render(request,"index.html")
+def student(request,name):
+    if name in ['A22126511167','A22126511134','A22126511135']:
+        return HttpResponse('<h1>creator</h1><h1 style="color: green; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 300px;">91%</h1>')
+    return HttpResponse('<h1 style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 100px;">75%</h1>')
+def edit(request):
+    return render(request,"edit.html")
