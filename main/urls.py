06a2d6772ef905ebc0710560d78281498ce2afc5
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-11-16 18:54:11 +0530

    connection bettwen two pages added' ;

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name="index"),
-    path('edit',views.edit,name="edit"),
+    path('',views.home,name="home"),
+    path('login',views.login,name="login"),
     path('<str:name>',views.student,name="report"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import *
-def index(request):
-    return render(request,"index.html",{
-        "wrong": False
-    })
+def login(request):
+
+    if 'id' in request.session:
+        return redirect(reverse("home"))
+    else:
+        return render(request,"index.html",{
+        "wrong": request.session['wrongp']
+        })
 def student(request,name):
     if name in ['A22126511167','A22126511134','A22126511135']:
         return HttpResponse('<h1>creator</h1><h1 style="color: green; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 300px;">91%</h1>')
     return HttpResponse('<h1 style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 100px;">75%</h1>')
-def edit(request):
-    if request.method=="GET":
+def home(request):
+    if request.method=='GET':
         if "id" not in request.session:
-            return  redirect('index')
+            request.session["wrongp"]=False
+            return  redirect('login')
+        else:
+            faculty=Faculty.objects.get(id=request.session['id'])
+            return render(request, "home.html", {
+                    "nums": Alloted.objects.filter(faculty=faculty),
+                })
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('pass')
-
         try:
             faculty = Faculty.objects.get(email=email,password=password)
-            return render(request, "edit.html", {
+            request.session['id']=faculty.id
+            print(request.session['id'])
+            return render(request, "home.html", {
                     "nums": Alloted.objects.filter(faculty=faculty),
                 })
         except Faculty.DoesNotExist:
-            return render(request, "index.html", {
-                "wrong": True
-            })
-    
-    return render(request, "index.html")
+            request.session['wrongp']=True
+            return redirect('login')
+    return redirect('login')
