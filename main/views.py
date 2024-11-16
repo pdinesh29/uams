a8ee01c404a16a3aa3aace9541d01880392f078f
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-11-16 20:35:04 +0530

    error fix

--- main/views.py
+++ main/views.py
@@ -6,9 +6,7 @@ def login(request):
     if 'id' in request.session:
         return redirect(reverse("home"))
     else:
-        return render(request,"index.html",{
-        "wrong": request.session['wrongp']
-        })
+        return render(request,"index.html")
 def student(request,name):
     if name in ['A22126511167','A22126511134','A22126511135']:
         return HttpResponse('<h1>creator</h1><h1 style="color: green; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 300px;">91%</h1>')
@@ -40,7 +38,7 @@ def home(request):
 def logout(request):
     request.session.flush()
     return redirect('home')
-def modifyy(request):
-    return render(request,'modifyy.html')
-def createe(request):
-    return render(request,'createe.html')
+# def modifyy(request):
+#     return render(request,'modifyy.html')
+# def createe(request):
+#     return render(request,'createe.html')
